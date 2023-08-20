0f9dd3340b06029b391df20e5e5d8b094c16f196
Daniel Osawaru <35234154+dosawaru@users.noreply.github.com>
2023-08-19 23:48:51 -0400

    show map view only

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import TabNavigation from './app/Screens/Navigations/TabNavigation';
+import GoogleMaps from './app/Screens/Components/GoogleMaps';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <TabNavigation>
+    // <NavigationContainer>
+    //   <TabNavigation>
         
-      </TabNavigation>
-    </NavigationContainer>
+    //   </TabNavigation>
+    // </NavigationContainer>
+    <GoogleMaps/>
   );
 }
 
