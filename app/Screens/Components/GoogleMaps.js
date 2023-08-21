d74fce648e89a3175aa0fad67b735c786b2f624a
Daniel Osawaru <35234154+dosawaru@users.noreply.github.com>
2023-08-20 21:05:45 -0400

    added clickable poi (google maps)

--- app/Screens/Components/GoogleMaps.js
+++ app/Screens/Components/GoogleMaps.js
@@ -3,7 +3,8 @@ import { Dimensions, StyleSheet, Text, View, ActivityIndicator } from "react-nat
 import MapView, { Callout, Circle, Marker, PROVIDER_GOOGLE } from "react-native-maps"
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import {NEXT_PUBLIC_GOOGLE_MAPS_API_KEY} from '@env'
-import {CustomMapStyle} from './../../../CustomMapStyle'
+//import {CustomMapStyle} from '../../../DarkMapStyle'
+import {CustomMapStyle} from '../../../NightMapStyle'
 import * as Location from 'expo-location';
 import LottieView from "lottie-react-native";
 
@@ -22,6 +23,7 @@ export default function GoogleMaps() {
 	const [calloutVisible, setCalloutVisible] = React.useState(false);
 	const [poiName, setPoiName] = React.useState('');
 	const [poiAddress, setPoiAddress] = React.useState('');
+	const [poiHours, setPoiHours] = React.useState('');
 
 	const mapViewRef = React.createRef();
 
@@ -74,31 +76,65 @@ export default function GoogleMaps() {
 		longitudeDelta: delta
 	});
 
-	//moves pin after being dragged
-    const handlePinDragEnd = (e) => {
+	////moves pin after being dragged
+    // const handlePinDragEnd = (e) => {
+	// 	setPin({
+	// 		latitude: e.nativeEvent.coordinate.latitude,
+	// 		longitude: e.nativeEvent.coordinate.longitude
+	// 	});
+	// };
+
+	////moves pin while scrolling through the maps
+	// const handleRegionChange = (newRegion) => {
+	// 	setRegion(newRegion);
+	// 	setPin({
+    //         latitude: newRegion.latitude,
+    //         longitude: newRegion.longitude
+    //     });
+	// };
+
+	////moves pin on long press
+	// const handleMapLongPress = (e) => {
+	// 	const newPin = {
+	// 	  latitude: e.nativeEvent.coordinate.latitude,
+	// 	  longitude: e.nativeEvent.coordinate.longitude,
+	// 	};
+	// 	setPin(newPin);
+	// 	setRegion(newPin);
+	// };
+
+	const handlePoiClick = (event, details) => {
+
+		const { placeId, name } = event.nativeEvent;
+		const { coordinate } = event.nativeEvent;
+ 		const { latitude, longitude } = coordinate;
+		
+		// Log POI information to the console
+		console.log('Place ID:', placeId);
+		console.log('Name:', name);
+		console.log('-----------------');
+
+		//Updates region and pin
+		//setRegion(selectedRegion);
 		setPin({
-			latitude: e.nativeEvent.coordinate.latitude,
-			longitude: e.nativeEvent.coordinate.longitude
+			latitude: latitude,
+			longitude: longitude,
 		});
-	};
 
-	//moves pin while scrolling through the maps
-	const handleRegionChange = (newRegion) => {
-		setRegion(newRegion);
-		setPin({
-            latitude: newRegion.latitude,
-            longitude: newRegion.longitude
-        });
-	};
-
-	//moves pin on long press
-	const handleMapLongPress = (e) => {
-		const newPin = {
-		  latitude: e.nativeEvent.coordinate.latitude,
-		  longitude: e.nativeEvent.coordinate.longitude,
+		const selectedRegion = {
+			latitude: latitude,
+			longitude: longitude,
+			latitudeDelta: delta,
+			longitudeDelta: delta,
 		};
-		setPin(newPin);
-		setRegion(newPin);
+ 
+		//animation to move mapview to new loaction
+		mapViewRef.current.animateToRegion(selectedRegion,4000);	
+
+		setPoiName(name);
+
+		// Shows callout
+		setCalloutVisible(true);
 	};
 
 	const handlePlaceSelected = (data, details) => {
@@ -112,7 +148,6 @@ export default function GoogleMaps() {
 
 		//animation to move mapview to new loaction
 		mapViewRef.current.animateToRegion(selectedRegion,4000);
-		mapViewRef.current.animateCamera({ center: selectedRegion, altitude: 8000 }, { duration: 4000 });
 		
 		//Updates region and pin
 		setRegion(selectedRegion);
@@ -124,10 +159,12 @@ export default function GoogleMaps() {
 		// Extract and set POI information
 		const poiName = details.name;
 		const poiAddress = details.formatted_address;
+		const poiHours = details.current_opening_hours.weekday_text.join('\n');
 	
 		// Update variables
 		setPoiName(poiName);
 		setPoiAddress(poiAddress);
+		setPoiHours(poiHours);
 	
 		// Shows callout
 		setCalloutVisible(true);
@@ -142,9 +179,9 @@ export default function GoogleMaps() {
 		console.log('Name:', name);
 		console.log('Street Address:', streetAddress);
 		console.log('Current Opening Hours:', currentOpeningHours);
-		
 	}
 
+	//starts animation
 	React.useEffect(() => {
 		if (animationRef.current) {
 			setTimeout(() => {
@@ -156,7 +193,6 @@ export default function GoogleMaps() {
 
 	//loading screen added to update map with users loaction
 	if (loading) {
-
 		return (
 			<View style={styles.loadingContainer}>
 			  	<View style={styles.animationContainer}>
@@ -203,41 +239,45 @@ export default function GoogleMaps() {
 				}}
 				style={styles.searchBar}
 				onPress={handlePlaceSelected} //updates loaction based on google search
+				
 
 			/>
 			<MapView
-				ref={mapViewRef} // Assign the ref to the MapView
+			 	//userInterfaceStyle={'dark'} 	//dark mode for iOS
+				ref={mapViewRef} 				// Assign the ref to the MapView
 				style={styles.map}
-				initialRegion={initialRegion} // Inital location on new load
+				initialRegion={initialRegion} 	// Inital location on new load
 				//onLongPress={handleMapLongPress}
 				onPress={() => {setCalloutVisible(true);}}
 				//onRegionChange={handleRegionChange} 
-				//provider={PROVIDER_GOOGLE}
-				//customMapStyle={CustomMapStyle}
+				provider={PROVIDER_GOOGLE}
+				customMapStyle={CustomMapStyle}
+				onPoiClick={handlePoiClick}
 
 			>
-                {pinVisible && ( // Show the pin if pinVisible is true
+                {pinVisible && ( 				// Show the pin if pinVisible is true
 					<Marker
-						coordinate={pin} 	//set pin to coordinates
-						pinColor= "#FC0FC0" //pink
-						draggable={false}	//ability to drag pin around
+						coordinate={pin} 		//set pin to coordinates
+						pinColor= "#FC0FC0" 	//pink
+						draggable={false}		//ability to drag pin around
 						//onDragEnd={handlePinDragEnd}
 					>
 						<Callout>
-							{calloutVisible && ( //displays if true
+							{calloutVisible && (//displays if true
 								<View style={styles.calloutContent}>
 									<Text style={styles.calloutText}>Name: {poiName}</Text>
 									<Text style={styles.calloutText}>Address: {poiAddress}</Text>
+									<Text style={styles.calloutText}>Hours: {poiHours}</Text>
 								</View>
 							)}
 						</Callout>
 					</Marker>
 				)}
-				<Circle center={pin} radius={1000} fillColor="rgba(255, 192, 203, 0.3)" strokeColor="grey"/> 
+				<Circle center={pin} radius={5000} fillColor="rgba(255, 192, 203, 0.3)" strokeColor="grey"/> 
 
 				{/* shows current coordinate */}
 				<Text style={styles.text}>Current latitude: {lat.toFixed(3)}{'\n'}</Text>
-      			<Text style={styles.text}>Current longitude: {long.toFixed(3)}</Text>
+				<Text style={styles.text}>Current longitude: {long.toFixed(3)}</Text>
 			</MapView>
 		</View>
 	)
@@ -254,18 +294,20 @@ const styles = StyleSheet.create({
     	alignItems: "center",
 	},
 	text: {
-    	alignItems: 'center',
 		justifyContent: 'center',
+        alignItems: 'center',
 		position: "absolute",
 		bottom: 0,
 		marginBottom: 20,
-		backgroundColor: 'lightgrey',
-		fontSize: 20,
+		fontSize: 25,
+		fontWeight: '100',
+		backgroundColor: 'lightgrey'
 	},
     loadingContainer: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+		backgroundColor: 'grey',
     },
 	animationContainer: {
         justifyContent: 'center',
@@ -280,7 +322,7 @@ const styles = StyleSheet.create({
 	loadingText: {
 		marginTop: 10,
 		color: '#333',
-		fontSize: 16,
+		fontSize: 18,
 	},
 	calloutContent: {
         maxHeight: 1000, 
@@ -289,5 +331,6 @@ const styles = StyleSheet.create({
     calloutText: {
         fontSize: 16,
         marginVertical: 4,
+		paddingLeft: 10,
     },
 })
